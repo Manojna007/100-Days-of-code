01f8a29daa8418c5370ca01a7277b50be9b631ff
Manojna007 <73425819+Manojna007@users.noreply.github.com>
2022-03-18 22:20:59 +0530

    Create 002.cpp

---
+++ 002.cpp
@@ -0,0 +1,28 @@
+/*
+Given two stringsaandbconsisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other.
+
+Example 1:
+
+Input:a = geeksforgeeks, b = forgeeksgeeks
+Output: YES
+Explanation: Both the string have samecharacters with
+        same frequency. So, both are anagrams.
+*/
+
+
+
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b){
+        
+        // Your code here
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        if(a==b)
+            return true;
+        return false;
+    }
+
+};
